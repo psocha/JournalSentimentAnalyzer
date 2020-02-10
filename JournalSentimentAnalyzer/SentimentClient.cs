a1d66ee2ee85bc95779bc0bcfbe4de8b18a8eee6
Peter Socha <psocha@users.noreply.github.com>
2020-02-09 21:28:50 -0800

    Functional text analytics client

--- JournalSentimentAnalyzer/SentimentClient.cs
+++ JournalSentimentAnalyzer/SentimentClient.cs
@@ -1,23 +1,48 @@
-﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
-using Microsoft.Rest;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
 
 namespace JournalSentimentAnalyzer
 {
     public class SentimentClient
     {
+        private const string key = "YOUR_KEY_HERE";
+        private const string endpoint = "YOUR_ENDPOINT_HERE";
+
+        private TextAnalyticsClient client;
+
         public SentimentClient()
         {
+            ApiKeyServiceClientCredentials credentials = new ApiKeyServiceClientCredentials(key);
+            this.client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = endpoint
+            };
         }
 
         public double GetSentimentScore(string text)
         {
-            return (double)text.Split(' ').Length / (double)100;
+            int retryCount = 0;
+            while (retryCount < 3)
+            {
+                try
+                {
+                    var sentimentResult = client.Sentiment(text);
+                    return sentimentResult.Score ?? 0.5;
+                }
+                catch (TaskCanceledException)
+                {
+                    retryCount++;
+                    Console.WriteLine($"TaskCanceledException, retry #{retryCount}");
+                }
+            }
+
+            Console.Error.WriteLine("Retry limit reached, returning neutral score");
+            return 0.5;
         }
     }
 }
